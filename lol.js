0a8b10b53f825ac55d120b7af91362cff7fd0487
Andrew Kane <codemaster@quantumraiders.com>
2013-12-10 12:11:45 -0800

    Added masteries, runes, leagues, teams.

--- lol.js
+++ lol.js
@@ -78,7 +78,16 @@ function getChampions(key, region, cb) {
 	return;
 }
 
-function getSummoner(key, region, name, cb) {
+function getSummoner(key, region, data, cb) {
+	if('id' in data) {
+		return getSummonerById(key, region, data['id'], cb);
+	} else if('name' in data) {
+		return getSummonerByName(key, region, data['name'], cb);
+	}
+	return false;
+}
+
+function getSummonerByName(key, region, name, cb) {
 	if(!region) region = 'na';
 	if(!checkKey(key, cb)) return;
 	request(apiUrl('/api/lol/' + region + '/v1.1/summoner/by-name/' + name, key), function(err, response, data) {
@@ -92,6 +101,108 @@ function getSummoner(key, region, name, cb) {
 	return;
 }
 
+function getSummonerById(key, region, id, cb) {
+	if(!region) region = 'na';
+	if(!checkKey(key, cb)) return;
+	request(apiUrl('/api/lol/' + region + '/v1.1/summoner/' + id, key), function(err, response, data) {
+		if(checkError(err, data, cb) && checkResponseStatus(response, data, cb)) {
+			var summoner = JSON.parse(data);
+			if(cb) cb(null, summoner);
+			return summoner;
+		}
+		return;
+	});
+	return;
+}
+
+function getSummonerNamesFromIds(key, region, ids, cb) {
+	if(!region) region = 'na';
+	if(!checkKey(key, cb)) return;
+	if(Array.isArray(ids)) {
+		ids = ids.toString()
+	}
+	request(apiUrl('/api/lol/' + region + '/v1.1/summoner/' + ids + '/name', key), function(err, response, data) {
+		if(checkError(err, data, cb) && checkResponseStatus(response, data, cb)) {
+			var summoners = JSON.parse(data)['summoners'];
+			if(cb) cb(null, summoners);
+			return summoners;
+		}
+		return;
+	});
+	return;
+}
+
+function getRunes(key, region, summoner, cb) {
+	if(!region) region = 'na';
+	if(!checkKey(key, cb)) return;
+	id = (summoner instanceof Object) ? summoner['id'] : summoner;
+	request(apiUrl('/api/lol/' + region + '/v1.1/summoner/' + id + '/runes', key), function(err, response, data) {
+		if(checkError(err, data, cb) && checkResponseStatus(response, data, cb)) {
+			var runes = JSON.parse(data)['pages'];
+			if(cb) cb(null, runes);
+			return runes;
+		}
+		return;
+	});
+	return;
+}
+
+function getMasteries(key, region, summoner, cb) {
+	if(!region) region = 'na';
+	if(!checkKey(key, cb)) return;
+	id = (summoner instanceof Object) ? summoner['id'] : summoner;
+	request(apiUrl('/api/lol/' + region + '/v1.1/summoner/' + id + '/masteries', key), function(err, response, data) {
+		if(checkError(err, data, cb) && checkResponseStatus(response, data, cb)) {
+			var runes = JSON.parse(data)['pages'];
+			if(cb) cb(null, runes);
+			return runes;
+		}
+		return;
+	});
+	return;
+}
+
+function getTeams(key, region, summoner, cb) {
+	if(!region) region = 'na';
+	if(!checkKey(key, cb)) return;
+	id = (summoner instanceof Object) ? summoner['id'] : summoner;
+	request(apiUrl('/api/' + region + '/v2.1/team/by-summoner/' + id, key), function(err, response, data) {
+		if(checkError(err, data, cb) && checkResponseStatus(response, data, cb)) {
+			var teams = JSON.parse(data);
+			if(cb) cb(null, teams);
+			return teams;
+		}
+		return;
+	});
+	return;
+}
+
+function getLeagues(key, region, summoner, cb) {
+	if(!region) region = 'na';
+	if(!checkKey(key, cb)) return;
+	id = (summoner instanceof Object) ? summoner['id'] : summoner;
+	request(apiUrl('/api/' + region + '/v2.1/league/by-summoner/' + id, key), function(err, response, data) {
+		if(checkError(err, data, cb) && checkResponseStatus(response, data, cb)) {
+			var leagues = []
+			var league_data = JSON.parse(data);
+			Object.keys(league_data).forEach(function(key, i) {
+				leagues.push(league_data[key]);
+			});
+			if(cb) cb(null, leagues);
+			return leagues;
+		}
+		return;
+	});
+	return;
+}
+
 exports.getRegions = getRegions;
 exports.getChampions = getChampions;
 exports.getSummoner = getSummoner;
+exports.getSummonerByName = getSummonerByName;
+exports.getSummonerById = getSummonerById;
+exports.getSummonerNamesFromIds = getSummonerNamesFromIds;
+exports.getRunes = getRunes;
+exports.getMasteries = getMasteries;
+exports.getTeams = getTeams;
+exports.getLeagues = getLeagues;
